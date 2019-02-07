55c17a2421922ba7a2b9abc078af79448bf1448e
Sai Harshith <kancherla.sai@dal.ca>
2019-02-06 19:36:06 -0500

    Task 1

--- task1.py
+++ task1.py
@@ -1,13 +1,14 @@
-file= open("Street_Centrelines.csv","r")
-str = []
+file= open("Street_Centrelines.csv","rt")
 name = []
+str1 = []
 str2 = []
 str3 = []
+
 for line in file:
-  line=line.split(",")
-  str.append(line[2])
-  name.append(line[4])
-  str2.append(line[6])
-  str3.append(line[7])
+	line=line.split(",")
+	str.append(line[2])
+	name.append(line[4])
+  	str2.append(line[6])
+ 	str3.append(line[7])
 
 print(list(zip(str,name,str2,str3)))
